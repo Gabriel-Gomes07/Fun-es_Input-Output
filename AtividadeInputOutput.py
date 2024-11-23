a98b5c93e73feb6a180e5b9471156f0d9f39a859
Gabriel De Oliveira Gomes <bieloliver7979@gmail.com>
2024-11-23 17:04:20 -0300

    Add files via upload

---
+++ AtividadeInputOutput.py
@@ -0,0 +1,53 @@
+#EX. 1
+mensagem = 'Boas Vindas!'
+print(mensagem)
+
+#EX. 2
+numero = bool(7)
+print(numero)
+type(numero)
+
+#EX. 3
+numero1 = 7
+numero2 = 8
+print(numero1 * numero2)
+
+#EX. 4
+numero3 = 10
+numero4 = 2
+print(numero3 / numero4)
+
+#EX.5
+numero5 = 15
+numero6 = 14
+print(numero5 - numero6)
+
+#EX.6
+numero7 = 10
+numero8 = 7
+print(numero7 // numero8)
+
+#EX.7
+numero9 = 8
+numero10 = 9
+numero11 = 10
+numero12 = 11
+print(numero9 * numero10 * numero11 * numero12)
+
+#EX. 8
+numero13 = 4
+print(numero13 * 2)
+
+#EX. 9
+print ('Calculadora')
+num1 = int(input('Primeiro número:'))
+num2 = int(input('Segundo número:'))
+resultado = print (num1 + num2)
+
+#EX. 10
+print('Faça seu cadastro')
+nome = str(input('Digite seu nome:'))
+sobrenome = str(input('Digite seu sobrenome:'))
+idade = int(input('Digite sua idade:'))
+email = str(input('Digite seu melhor email:'))
+telefone = str(input('Digite o seu número de telefone:'))
